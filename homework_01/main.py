db06de09c0f2a44645406de3f4ce07eb4a12710d
Marat Akhmetianov <marat.akhmetianov@gmail.com>
2021-03-10 16:10:30 +0300

    added files for HW-1

---
+++ homework_01/main.py
@@ -0,0 +1,40 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(*numbers):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    """
+    return [number ** 2 for number in numbers]
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
+def filter_numbers(numbers, filter_type):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+    """
+    if filter_type == ODD:
+        return [x for x in numbers if x % 2 == 1]
+    elif filter_type == EVEN:
+        return [x for x in numbers if x % 2 == 0]
+    elif filter_type == PRIME:
+        prime_list = []
+        for num in numbers:
+            if num > 1:
+                for i in range(2, num):
+                    if (num % i) == 0:
+                        break
+                else:
+                    prime_list.append(num)
+        return prime_list
